c353650f4204ffbec8fdbe7c748607760c417b72
Baptiste ZETLER <baptiste.zetler@epitech.eu>
2025-03-20 11:47:53 +0100

    init 4

--- main.c
+++ main.c
@@ -1,4 +1,11 @@
-int main(void)
-{
+// main.c
+#include <stdio.h>
+
+int addition(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    printf("Résultat : %d\n", addition(2, 3));
     return 0;
 }
--- test_main.c
+++ test_main.c
@@ -1,4 +1,11 @@
-int test(void)
-{
+// test_main.c
+#include <stdio.h>
+
+// Prototype de la fonction à tester (dans main.c)
+int addition(int a, int b);
+
+int main() {
+    printf("Test 1 + 2 = %d (attendu 3)\n", addition(1, 2));
+    printf("Test 5 + 5 = %d (attendu 10)\n", addition(5, 5));
     return 0;
 }
